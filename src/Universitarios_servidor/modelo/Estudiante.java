7e6c30425f77ab4cf1fd39bd13a759e91a384418
APP DISTRIBUIDAS <jonathanjairxd02@gmail.com>
2025-05-12 15:30:15 -0500

    Parte practica de la prueba N 1

---
+++ src/Universitarios_servidor/modelo/Estudiante.java
@@ -0,0 +1,22 @@
+package Universitarios_servidor.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class Estudiante {
+    @Getter @Setter private int id;
+    @Getter @Setter private String nombre;
+    @Getter @Setter private String telefono;
+    @Getter @Setter private String carrera;
+    @Getter @Setter private int semestre;
+    @Getter @Setter private boolean gratuidad;
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nNombre: " + nombre + "\nTeléfono: " + telefono +
+                "\nCarrera: " + carrera + "\nSemestre: " + semestre +
+                "\nGratuidad: " + (gratuidad ? "Sí" : "No");
+    }
+}
+
---
+++ src/Universitarios_servidor/test/Test.java
@@ -0,0 +1,10 @@
+package Universitarios_servidor.test;
+
+import Universitarios_servidor.servicio.Servidor;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Servidor servidor = new Servidor();
+        servidor.servicio();
+    }
+}
